eae15bb82cb98846bd902ede6e6eae312cd61c02
lSylra <143262189+lSylra@users.noreply.github.com>
2023-11-29 21:24:09 -0500

    Add files via upload

---
+++ Actividad6.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.actividad6;
+
+/**
+ *
+ * @author USER
+ */
+public class Actividad6 {
+
+    public static void main(String[] args) {
+        Principal form = new Principal();
+        form.setVisible(true);
+    }
+}
